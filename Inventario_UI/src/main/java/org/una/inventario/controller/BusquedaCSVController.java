675292e0f798a6b57e72448a33ed628e893f65b7
JIMMM1212 <juan.morales.mora@est.una.ac.cr>
2021-11-01 18:14:58 -0600

    CargarArchivo y archivo - CSV

--- Inventario_UI/src/main/java/org/una/inventario/controller/BusquedaCSVController.java
+++ Inventario_UI/src/main/java/org/una/inventario/controller/BusquedaCSVController.java
@@ -1,8 +1,98 @@
 package org.una.inventario.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import org.una.inventario.util.Mensaje;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BusquedaCSVController extends Controller{
+    @FXML
+    public JFXButton btnBuscar;
+    @FXML
+    public Pane pPane;
+    @FXML
+    public ListView ListViewActivos;
+
+    private Mensaje msg = new Mensaje();
+
     @Override
     public void initialize() {
 
+    }
+
+    public void onActionBuscar(ActionEvent actionEvent) {
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("CSV Files","*.csv"));
+        List<File> selectedFiles = (List<File>) fc.showOpenMultipleDialog(null);
+
+        String dErrores = "";
+        if(selectedFiles != null){
+            for(int i = 0; i < selectedFiles.size(); i++){
+                if(validarArchivo(selectedFiles.get(i))){
+                    ListViewActivos.getItems().add(selectedFiles.get(i).getAbsoluteFile());
+                }
+                else{
+                    dErrores += selectedFiles.get(i).getName()+ " ";
+                    ListViewActivos.getItems().add(selectedFiles.get(i).getAbsoluteFile());
+                }
+            }
+        }
+
+        if(dErrores != ""){
+
+            msg.show(Alert.AlertType.INFORMATION, "Advertencia", "Los siguientes archivos presentan errores: "+dErrores);
+        }
+        /*
+        for(int i = 0 ; i < records.size(); i++){
+            System.out.println(records.get(i).get(0));
+        }
+         */
+    }
+
+    private boolean validarArchivo(File archivo){
+
+        List<List<String>> records = new ArrayList<List<String>>();
+        try (CSVReader csvReader = new CSVReader(new FileReader(archivo.getAbsoluteFile()));) {
+            String[] values = null;
+            while ((values = csvReader.readNext()) != null) {
+                records.add(Arrays.asList(values));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CsvValidationException e) {
+            e.printStackTrace();
+        }
+
+        String dato = records.get(0).get(0);
+        int cantidad = 0;
+        for(int i = 0; i < dato.length(); i++){
+            if(dato.charAt(i)== ';'){
+                cantidad++;
+            }
+        }
+
+        records.clear();
+
+        if(cantidad == 10){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
